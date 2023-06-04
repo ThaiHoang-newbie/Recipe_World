e0fd3ec58c1ceb48bc8b100238bd0fe1ed03050e
ThaiHoang-newbie <thaihoang20112k3@gmail.com>
2023-06-04 22:00:35 +0700

    Admin API(Posts and Obtainers)

--- back_end/routes/api.php
+++ back_end/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Get all obtainers
+Route::get('/getAllObtainers',[ApiController::class,'getAllObtainer']);
+
+// Get a obtainer
+Route::get('/getObtainerById/{id}',[ApiController::class,'getObtainerById']);
+
+
+
+// Get all posts by id
+Route::get('/getAllPosts',[ApiController::class,'getAllPost']);
+
+// Get posts by obtainer_id
+Route::get('/getPostByObtainerId/{id}',[ApiController::class,'getPostByObtainerId']);
+
+// Get posts by obtainer_id
+Route::get('/getPostByCategoryId/{id}',[ApiController::class,'getPostByCategoryId']);
